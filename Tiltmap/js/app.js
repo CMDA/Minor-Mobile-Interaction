d504326962b1ae00cdb8cb56279adeb3c72bdc62
vijnv <vijnv@scherbatsky.home>
2014-02-21 21:39:06 +0100

    Vloeiendere kaartbeweging
    Het verschuiven van de kaart is nu soepeler door minder metingen per
    seconde te doen op de accelerometer maar per beweging de snelheid
    groter te maken.

--- Tiltmap/js/app.js
+++ Tiltmap/js/app.js
@@ -5,13 +5,15 @@
 	var settings = {
         mapID: 'map',
         location: [51.505, -0.09],
-        zoom: 13
+        zoom: 13,
+        treshold: 2,
+        speed: 50
 	};
 
 	var controller = {
 		init: function () {
 			console.log('init');
-            document.addEventListener('touchmove',  function(e){ e.preventDefault() }, false);
+            document.addEventListener('touchmove',  function(e){ e.preventDefault(); }, false);
             map.init();
             accel.startWatch();
             navigator.splashscreen.hide();
@@ -20,7 +22,7 @@
 
 	var accel = {
         watchID: null,
-        options: {frequency: 10},
+        options: {frequency: 100},
         
         startWatch: function () {
             this.watchID = navigator.accelerometer.watchAcceleration(this.processData, this.onError, this.options);
@@ -30,18 +32,18 @@
             var movX;
             var movY;
             
-            if (acceleration.x > 2) {
-                movX = 2;
-            } else if (acceleration.x < -2) {
-                movX = -2;
+            if (acceleration.x > settings.treshold) {
+                movX = settings.speed;
+            } else if (acceleration.x < settings.treshold*-1) {
+                movX = settings.speed*-1;
             } else {
                 movX = 0;
             }
             
-            if (acceleration.y > 2) {
-                movY = -2;
-            } else if (acceleration.y < -2) {
-                movY = 2;
+            if (acceleration.y > settings.treshold) {
+                movY = settings.speed*-1;
+            } else if (acceleration.y < settings.treshold*-1) {
+                movY = settings.speed;
             } else {
                 movY = 0;
             }
@@ -60,7 +62,8 @@
         mapObject: null,
         
         init: function () {
-            this.mapObject = L.map(settings.mapID, {zoomControl: false, attributionControl: false}).setView(settings.location, settings.zoom);
+            console.log('init map');
+            this.mapObject = L.map(settings.mapID, {zoomControl: false, attributionControl: false, dragging: false}).setView(settings.location, settings.zoom);
             var layerOptions = {attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'};
             L.tileLayer('http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.jpg', layerOptions).addTo(this.mapObject);
         }
